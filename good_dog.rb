a10b0eca0c549af6a86be6781a91d08f23fd7fd0
Max Diak <max@via-prima.com>
2015-06-27 22:12:33 +0400

    Class's self. prefix in use

--- good_dog.rb
+++ good_dog.rb
@@ -1,26 +1,38 @@
 # good_dog.rb
 
 class GoodDog
-  attr_accessor :name
+  attr_accessor :name, :height, :weight
 
-  def initialize(name)
-    @name = name
+  def initialize(n, h, w)
+    @name = n
+    @height = h
+    @weight = w
   end
 
   def speak
-    "#{@name} says: Arf!"
+    "#{name} says: Arf!"
+  end
+
+  def change_info(n, h, w)
+    self.name = n
+    self.height = h
+    self.weight = w
+  end
+
+  def info
+    "#{self.name} weights #{self.weight} and is #{self.height} tall."
   end
 
 end
 
-sparky = GoodDog.new('Sparky')
+sparky = GoodDog.new('Sparky', '12 inches', '10 lbs')
 puts sparky.speak
 
-fido = GoodDog.new("Fido")
+fido = GoodDog.new('Fido', '30 inches', '60 lbs')
 puts fido.speak
 
-puts sparky.name
-puts fido.name
+puts sparky.info
+puts fido.info
 
-sparky.name = "Spartakus"
-puts sparky.name
+sparky.change_info("Spartakus", "24 inches", "45 lbs")
+puts sparky.info
